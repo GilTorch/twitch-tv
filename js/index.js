9f409ac63fccd2e54ff7f5a572acf3b826c2577f
Gilbert <gilbert.torchon@esih.edu>
2018-03-07 09:58:39 -0500

    added download animation. refactored the old functions

--- js/index.js
+++ js/index.js
@@ -1,154 +1,155 @@
-$(document).ready(function(){
- 
- loadAllChannels();
- $("#all-offline-online").click(function(){
-   loadAllChannels();
- })
-  
-$("#online-channels").click(function(){
-  console.log("online called");
-   loadOnlineChannels();
- })
-  
 
-$("#offline-channels").click(function(){
-   loadOfflineChannels();
- })
-  
+var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-  
+$(document).ready(function () {
+  loadAllChannels(users);
+  $("#all-offline-online").click(function () {
+    $("#loading-animation").show();    
+    loadAllChannels(users);
+  })
+
+  $("#online-channels").click(function () {
+    $("#loading-animation").show();    
+    console.log("online called");
+    loadOnlineChannels();
+  })
+
+
+  $("#offline-channels").click(function () {
+    $("#loading-animation").show();    
+    loadOfflineChannels();
+  })
+
 })
 
 
-function loadOfflineChannels(){
-   $("#show-channels").empty();
- var channelsIWannaSee=["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-  for(var i=0;i<channelsIWannaSee.length;i++)
-        {
-                     
-                       $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/"+channelsIWannaSee[i]).done(function(data){
-                         
-              
-                        if(data.stream==null)
-                          {
-                            
-                            var url=data._links.channel;
-                            url=url.replace("https://api.twitch.tv/kraken/","https://wind-bow.gomix.me/twitch-api/");
-                            
-                           // alert(typeof url);
-                            //console.log(url);
-                            
-                            $.getJSON(url).done(function(newdata)
-                                               {
-                             // alert(newdata);
-                                var h1=$("<h1></h1>").text("[OFFLINE]");
-                               h1.css('color','red');
-                                var logo= $("<img/>").attr("src",newdata.logo);
-                               var status= $("<p></p>").text(newdata.status);
-                               var div= $("<div></div>");
-                              div.append(h1);
-                                 div.append(logo);
-                              // div.append(status);
-                                  var link=$("<a class='each-channel' target='_blank'></a>").attr("href",newdata.url);
-                                link.css('display','block');
-                                 link.append(div);
-                                link.css('opacity','0.4');
-                               $("#show-channels").append(link);
-                              
-                              
-                            })
-                         }
-                        
-                                                            
-                                                                                                         
-                   });
-            }
-            $("#myAnimation").hide();
-        }
+function loadOfflineChannels() {
+  $("#show-channels").empty();
+  users.forEach(function (user) {
+    $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + user + "?callback=?").done(function (data) {
+      var dataToFormat = {
+        "logo": "NO LOGO",
+        "status": "NO STATUS",
+        "channel": "#",
+        "connexionStatus": "OFFLINE"
+      }
+      if (!data.stream) {
+        $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/" + user + "?callback=?").done(function (channeldata) {
+          console.log(channeldata.logo);
+          dataToFormat.logo = channeldata.logo;
+          dataToFormat.status = channeldata.status;
+          formatData(dataToFormat);
+        });
+      $("#loading-animation").hide();
+        
+      }
+    });
+  })
+}
 
 
+function loadOnlineChannels() {
+  $("#show-channels").empty();
+  users.forEach(function (user) {
+    $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + user + "?callback=?").done(function (data) {
+      var dataToFormat = {
+        "logo": "NO LOGO",
+        "status": "NO STATUS",
+        "channel": "#",
+      }
+      if (data.stream) {
+        dataToFormat.logo = data.stream.channel.logo;
+        dataToFormat.status = data.stream.channel.status;
+        dataToFormat.channel = data.stream.channel.url;
+        dataToFormat.connexionStatus = "ONLINE";
+        formatData(dataToFormat);
+      $("#loading-animation").hide();
+        
+      }
+    });
+  })
+
+}
 
 
-function loadOnlineChannels(){
-   $("#show-channels").empty();
-   var animation=$("<div></div>");
- var channelsIWannaSee=["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-  for(var i=0;i<channelsIWannaSee.length;i++)
-        {
-                       $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/"+channelsIWannaSee[i]).done(function(data){
-                         
-              
-                        if(data.stream!==null)
-                          {  
-                                var h1=$("<h1></h1>").text("[ONLINE]");
-                                h1.css('color','green');
-                                var logo= $("<img/>").attr("src",data.stream.channel.logo);
-                               var status= $("<p></p>").text(data.stream.channel.status);
-                               var div= $("<div></div>");
-                               div.append(h1);
-                                 div.append(logo);
-                               div.append(status);
-                                  var link=$("<a class='each-channel' target='_blank'></a>").attr("href",data.stream.channel.url);
-                                link.css('display','block');
-                                 link.append(div);
-                               $("#show-channels").append(link);
+function loadAllChannels(users) {
+  $("#show-channels").empty();
+  users.forEach(function (user) {
+    $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + user + "?callback=?").done(function (data) {
+      var dataToFormat = {
+        "logo": "NO LOGO",
+        "status": "NO STATUS",
+        "channel": "#",
+        "connexionStatus": "OFFLINE"
+      }
+      if (!data.stream) {
+        $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/" + user + "?callback=?").done(function (channeldata) {
+          console.log(channeldata.logo);
+          dataToFormat.logo = channeldata.logo;
+          dataToFormat.status = channeldata.status;
+          formatData(dataToFormat);
+        });
+      } else {
+        dataToFormat.logo = data.stream.channel.logo;
+        dataToFormat.status = data.stream.channel.status;
+        dataToFormat.channel = data.stream.channel.url;
+        dataToFormat.connexionStatus = "ONLINE";
+        formatData(dataToFormat);
+      }
+      $("#loading-animation").hide();
+    });
+  })
+
+
+}
+
+
+function formatData(data) {
+  console.log(data,data.status);
+  var connexionStatusContainer = $("<h1></h1>");
+  var logoContainer = $("<img/>");
+  var statusContainer = $("<p></p>");
+  var container = $("<div></div>");
   
-                          }
-                                                            
-                                                                                                         
-                   });
-            }
-             $("#myAnimation").hide();
-        }
+  if(data.connexionStatus==="OFFLINE"){
+    container.addClass("offline");
+  }else{
+    container.addClass("online");
+  }
+  connexionStatusContainer.text("["+data.connexionStatus+"]");
+  logoContainer.attr('src',data.logo);
+  statusContainer.text(data.status);
+  container.append(connexionStatusContainer);
+  container.append(logoContainer);
+  container.append(statusContainer);
+  var link = $("<a class='each-channel' target='_blank'></a>").attr("href", data.channel);
+  link.css('display', 'block');
+  link.append(container);
+  $("#show-channels").append(link);
+}
 
 
-function loadAllChannels(){
-   $("#show-channels").empty();
- var channelsIWannaSee=["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas","brunofin","comster404"];
-  for(var i=0;i<channelsIWannaSee.length;i++)
-        {
-                       $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/"+channelsIWannaSee[i]).done(function(data){
-                         
-                                    
-                                var streamurl=data.url;
-                                streamurl=streamurl.replace("https://www.twitch.tv","https://wind-bow.gomix.me/twitch-api/streams");
-                           
-                               $.getJSON(streamurl).done(function(streamdata){
-                                      
-                                      if(streamdata.stream!==null)
-                                       {
-                                              var h1=$("<h1></h1>").text("[ONLINE]");
-                                              h1.css('color','green');
-                                             var logo= $("<img/>").attr("src",data.logo);
-                                             var status= $("<p></p>").text(data.status);
-                                             var div= $("<div></div>");
-                                              div.append(h1);
-                                               div.append(logo);
-                                             div.append(status);
-                                                var link=$("<a class='each-channel' target='_blank'></a>").attr("href",data.url);
-                                              link.css('display','block');
-                                               link.append(div);
-                                             $("#show-channels").append(link);
-                                       }
-                                 else{
-                                                 var h1=$("<h1></h1>").text("[OFFINE]");
-                                                h1.css('color','red');
-                                                 var logo= $("<img/>").attr("src",data.logo);
-                                                 var status= $("<p></p>").text(data.status);
-                                                 var div= $("<div></div>");
-                                                  div.append(h1);
-                                                  div.append(logo);
-                                                // div.append(status);
-                                                  var link=$("<a class='each-channel' target='_blank'></a>").attr("href",data.url);
-                                                  link.css('display','block');
-                                                  link.css('opacity','0.4');
-                                                  link.append(div);
-                                                  $("#show-channels").append(link);
-                                   
-                                 }
-                                 
-                               });                                                                          
-                   });
-            }
-             $("#myAnimation").hide();
-        }
+function loadChannel(){
+  $("#loading-animation").show();
+  var searchQuery=$("#search-box").val();
+  if(selectedUser===""){
+    loadAllChannels(users);
+  }
+  var selectedUser=users.filter(function(user){
+    return user.indexOf(searchQuery)!==-1;
+  });
+  loadAllChannels(selectedUser);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
